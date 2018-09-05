944a93823a3433ea62de889ff14e6b4d8d20eaa6
Olegzandr VD <alex@oresoftware.com>
2018-09-04 20:05:53 -0700

    ores/auto-commit => 'set'

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@
 
 // https://stackoverflow.com/questions/29775830/how-to-implement-a-typescript-decorator
 
+import * as assert from 'assert';
+
 export const r2gSmokeTest = function () {
   // r2g command line app uses this exported function
   return true;
@@ -56,14 +58,22 @@ export interface Parsed {
   [key: string]: { type: string, default: any, name: string, cleanName: string }
 }
 
+
+
 export class CliParser<T extends Array<ElemType>> {
   
   options: T;
   opts: OptionsToType<T>;
-  values: Array<string> = [];
   
   constructor(o: T) {
     this.options = o;
+    for(let i = 0; i < o.length; i++) {
+      const v = o[i];
+      if ('short' in v) {
+        assert(typeof v.short === 'string', '"short" property must be a string.');
+        assert(v.short && v.short.length  === 1, '"short" string must be one character in length.');
+      }
+    }
   }
   
   getCleanOpt(v: string) {
@@ -96,22 +106,22 @@ export class CliParser<T extends Array<ElemType>> {
   
   parse() {
     
-    const ret = {} as {[key:string]: any};
+    const ret = {} as { [key: string]: any };
+    const values: Array<string> = [];
     
-    const namesHash = <Parsed>{};
+    const nameHash = <Parsed>{};
     const shortNameHash = <Parsed>{};
     
     for (let i = 0; i < this.options.length; i++) {
       const o = this.options[i];
       const cleanName = this.getCleanOpt(o.name);
-      namesHash[cleanName] = {type: o.type, default: o.default, name: o.name, cleanName};
-      if(o.short){
-        shortNameHash[this.getCleanOpt(o.short)] = {type: o.type, default: o.default, name: o.name, cleanName};
+      nameHash[cleanName] = {type: o.type, default: o.default, name: o.name, cleanName};
+      if (o.short) {
+        shortNameHash[o.short] = {type: o.type, default: o.default, name: o.name, cleanName};
       }
     }
     
     const args = this.getSpreadedArray(process.argv.slice(2));
-    
     console.log('these args:', args);
     
     let prev = null;
@@ -121,68 +131,121 @@ export class CliParser<T extends Array<ElemType>> {
       const a = args[i];
       const clean = this.getCleanOpt(a);
       
+      if (prev) {
+        const name = prev.name;
+        ret[name] = a;
+        prev = null;
+        continue;
+      }
+      
+      if (!a.startsWith('-')) {
+        values.push(a);
+        prev = null;
+        continue;
+      }
+      
       let longOpt = null, shortOpts: Parsed = null;
       
       if (a.startsWith('--')) {
-        longOpt = namesHash[clean];
+        longOpt = nameHash[clean];
+        if (longOpt.type === Type.Boolean || longOpt.type === Type.ArrayOfBoolean) {
+          ret[longOpt.name] = true;
+        }
+        else {
+          prev = longOpt;
+          if(!args[i+1]){
+            throw new Error('Not enough arguments to satisfy:' + JSON.stringify(longOpt));
+          }
+        }
+        continue;
       }
-      else if (a.startsWith('-')) {
+      
+      if (a.startsWith('-')) {
         
-        const shorties = clean.split('');
+        const shorties = a.slice(1).split('');
         shortOpts = shorties.reduce((a, b) => (a[b] = shortNameHash[b], a), <Parsed>{});
         
         const keys = Object.keys(shortOpts);
+        
+        console.log('keys:', keys);
+        
         let moreThanOne = false;
         keys.forEach(k => {
+          
           const t = shortOpts[k];
-          if (moreThanOne === true && t.type !== Type.Boolean) {
-            throw new Error('You can only group boolean options, this is a problem => ' + a);
+          
+          if (t.type !== Type.Boolean && t.type !== Type.ArrayOfBoolean) {
+            if(moreThanOne === true){
+              throw new Error('You can only group boolean options, this is a problem => ' + a);
+            }
+            if(!args[i+1]){
+              throw new Error('Not enough arguments to satisfy:' + JSON.stringify(t));
+            }
           }
           
           const shortHashVal = shortNameHash[k];
           
-          if(!shortHashVal){
+          if (!shortHashVal) {
             throw new Error('Could not find option for shortname: ' + k);
           }
           
-          const longNameHashVal = namesHash[shortHashVal.cleanName];
+          const longNameHashVal = nameHash[shortHashVal.cleanName];
           
-          if(!longNameHashVal){
+          if (!longNameHashVal) {
             throw new Error('Could not find hash val for name: ' + longNameHashVal);
           }
           
           const originalName = longNameHashVal.name;
           
-          if(shortHashVal.type === Type.Boolean){
+          if (shortHashVal.type === Type.Boolean || shortHashVal.type === Type.ArrayOfBoolean) {
             ret[originalName] = true;
           }
-       
+          
           moreThanOne = true;
         });
         
       }
       
-      if (!a.startsWith('-')) {
-        this.values.push(a);
-        return;
-      }
-      
-      prev = a;
+      prev = longOpt || Object.values(shortOpts)[0];
+      console.log('prev:', prev);
       
     }
     
-    return ret;
+    return {
+      opts: <OptionsToType<T>>ret,
+      values
+    };
   }
 }
 
-const p = new CliParser(asOptions([{
-  name: 'foo',
-  short: 'a',
-  type: Type.Boolean
-}]));
+const p = new CliParser(asOptions([
+  {
+    name: 'foo:3',
+    short: 'a',
+    type: Type.Boolean
+  },
+  {
+    name: 'zoomBar',
+    type: Type.String
+  },
+  
+  {
+    name: 'tall',
+    short: 'X',
+    type: Type.String
+  },
+  
+  {
+    name: 'dog',
+    short: 'x',
+    type: Type.String
+  }
 
-const o = p.parse();
-console.log(o);
+]));
+
+const {opts, values} = p.parse();
+
+console.log(opts, values);
 
 
 
