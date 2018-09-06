1dd14f391373a583f988ba73c6437cf1dee18733
Olegzandr VD <alex@oresoftware.com>
2018-09-06 01:47:01 -0700

    ores/auto-commit => 'set'

--- src/exp.ts
+++ src/exp.ts
@@ -1,21 +1,31 @@
 'use strict';
 
-interface Elem<T = any> {
+type TypeMapping = {
+  Boolean: boolean,
+  String: string,
+  Number: number,
+  Integer: number,
+  ArrayOfString: Array<string>,
+  JSON: any,
+  SeparatedStrings: Array<string>,
+}
+
+export interface ElemType<T = any> {
   name: string,
-  type: string,
-  typeOverride?: T
+  type: keyof TypeMapping,
 }
 
-interface JSON {
+export const asOptions = <K extends keyof any, T extends Array<{ name: K, type: keyof TypeMapping }>>(t: T) => t;
 
-}
+export type OptionsToType<T extends Array<ElemType>>
+  = { [K in T[number]['name']]: TypeMapping[Extract<T[number], { name: K }>['type']] }
 
-const typeOverride = <T>(v: Elem): Elem<T> => <Elem<T>>v;
 
-const v: Array<Elem> = [
+const typeOverride = <T>(v: ElemType): ElemType<T> => <ElemType<T>>v;
+
+const v = asOptions([
   
   {name: 'foo', type: 'Boolean'},
-  
   {name: 'bar', type: 'String'},
   
   typeOverride<string>({
@@ -23,7 +33,15 @@ const v: Array<Elem> = [
     type: 'JSON'
   })
   
-];
+]);
+
+const opts = <OptionsToType<typeof v>>{foo: true, bar: 'ageage'};
+
+
+
+
+
+
 
 
 
